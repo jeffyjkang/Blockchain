3faf4f84f189717e8343eeb0b209846a83ccd0ba
jeffyjkang <jeffyjkang@gmail.com>
2019-10-02 17:33:21 -0700

    day1 complete

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -2,9 +2,23 @@ import hashlib
 import requests
 
 import sys
+import json
 
 
 # TODO: Implement functionality to search for a proof 
+
+def proof_of_work(block):
+    block_string = json.dumps(block, sort_keys=True).encode()
+    proof = 0
+    while valid_proof(block_string, proof) is False:
+        proof +=1
+    return proof
+
+def valid_proof(block_string, proof):
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == "000000"
+
 
 
 if __name__ == '__main__':
@@ -18,10 +32,22 @@ if __name__ == '__main__':
     # Run forever until interrupted
     while True:
         # TODO: Get the last proof from the server and look for a new one
+        r = requests.get(url=node + "/last_block")
+        data = r.json()
+        new_proof = proof_of_work(data.get('last_block'))
         # TODO: When found, POST it to the server {"proof": new_proof}
+        print("submitting proof: " + str(new_proof))
+        post_data = {"proof": new_proof}
         # TODO: We're going to have to research how to do a POST in Python
         # HINT: Research `requests` and remember we're sending our data as JSON
+        r = requests.post(url=node + "/mine", json=post_data)
+        data = r.json()
         # TODO: If the server responds with 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        if data.get('message') == 'New Block Forged':
+            coins_mined += 1
+            print("Total coins mined: " + str(coins_mined))
+        else:
+            print(data.get('message'))
+        # pass
